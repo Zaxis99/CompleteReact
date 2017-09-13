b88978bb8ca4a65c485d04897f6c2a3132b94e0d
Zack Harvey <zackh99@gmail.com>
2017-09-13 16:04:15 -0300

    Started on WeatherApp

---
+++ ReactWeather/app/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, BrowserRouter, IndexRoute, hashHistory} = require('react-router-dom');
+var Main = require('Main');
+
+ReactDOM.render(
+    <BrowserRouter history={hashHistory}>
+        <Route path="/" component={Main}>
+
+        </Route>
+    </BrowserRouter>,
+    document.getElementById('app')
+)
---
+++ ReactWeather/webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+    entry: './app/app.jsx',
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        alias: {
+            Main: path.resolve(__dirname, 'app/components/Main.jsx'),
+        },
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    }
+};
